7d760c609f8b3025c66e139547d776a3286ede59
mwong068 <mwong068@gmail.com>
2020-02-10 14:18:50 -0800

    stripe api is correctly working for front end checkout

--- src/components/checkout/CardSection.js
+++ src/components/checkout/CardSection.js
@@ -1,31 +1,86 @@
 
 import React from 'react';
-import {CardElement} from 'react-stripe-elements';
+import {CardElement, CardNumberElement, CardExpiryElement, CardCvcElement, PaymentRequestButtonElement} from 'react-stripe-elements';
+
+var inputs = document.querySelectorAll('.cell.example.example2 .input');
+  Array.prototype.forEach.call(inputs, function(input) {
+    input.addEventListener('focus', function() {
+      input.classList.add('focused');
+    });
+    input.addEventListener('blur', function() {
+      input.classList.remove('focused');
+    });
+    input.addEventListener('keyup', function() {
+      if (input.value.length === 0) {
+        input.classList.add('empty');
+      } else {
+        input.classList.remove('empty');
+      }
+    });
+  });
 
 const style = {
+  // base: {
+  //   color: "#32325d",
+  //   fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+  //   fontSmoothing: "antialiased",
+  //   fontSize: "16px",
+  //   width: '100px',
+  //   "::placeholder": {
+  //     color: "#aab7c4"
+  //   }
+  // },
+  // invalid: {
+  //   color: "#fa755a",
+  //   iconColor: "#fa755a"
+  // }
   base: {
-    color: "#32325d",
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: "antialiased",
-    fontSize: "16px",
-    width: '100px',
-    "::placeholder": {
-      color: "#aab7c4"
-    }
+    color: '#6A6C6E',
+    fontWeight: 500,
+    fontFamily: 'Source Code Pro, Consolas, Menlo, monospace',
+    fontSize: '16px',
+    fontSmoothing: 'antialiased',
+
+    '::placeholder': {
+      color: '#6A6C6E',
+    },
+    ':-webkit-autofill': {
+      color: '#e39f48',
+    },
   },
   invalid: {
-    color: "#fa755a",
-    iconColor: "#fa755a"
-  }
+    color: '#E25950',
+
+    '::placeholder': {
+      color: '#FFCCA5',
+    },
+  },
+};
+
+var elementClasses = {
+  focus: 'focus',
+  empty: 'empty',
+  invalid: 'invalid',
 };
 
 const CardSection = () => {
   return (
     <center>
-        <label>
-        Card details
-            <CardElement className="MyCardElement" style={style} />
+        <label id="checkout-label">
+        <h3>Payment Information</h3>
         </label>
+        <br></br>
+        <CardNumberElement className="MyCardElement" style={style} classes={elementClasses} />
+       <br></br>
+        <CardExpiryElement className="MyCardElement" style={style} classes={elementClasses} />
+       <br></br>
+        <CardCvcElement className="MyCardElement"  style={style} classes={elementClasses} />
+       <br></br>
+        {/* apple pay button - for later implementation */}
+          {/* <PaymentRequestButtonElement className="MyCardElement" /> */}
+        {/* one line card element */}
+        {/* <CardElement className="MyCardElement" style={style} /> */}
+        
     </center>
   );
 };
--- src/components/checkout/PaymentForm.js
+++ src/components/checkout/PaymentForm.js
@@ -1,76 +1,31 @@
 import React from 'react';
 import { injectStripe } from 'react-stripe-elements';
 import CardSection from './CardSection';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class PaymentForm extends React.Component {
-    //from first react stripe article
-    // state = {
-    //     email: ''    
-    // }
 
-    // async submit(event) {
-    //     let chargeToken = await this.props.stripe.createToken({name: "Name"})
-    //     const token = localStorage.token;
-
-    //     let charge = {
-    //         token: chargeToken.token.id
-    //     }
-    //     let response = await fetch('http://127.0.0.1:3000/charges', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Accept': 'application/json',
-    //             'Authorization': `Bearer ${token}`
-    //         }
-    //         body: JSON.stringify({
-    //             charge: charge
-    //         })
-    //     })
-
-    //     if (response.ok) console.log("Purchase Complete!")
-    // }
-
-    // render () {
-    //     return (
-    //         <div>
-
-    //         </div>
-    //     )
-    // }
-
-
-    //from stripe
-    // handleSubmit = (ev) => {
-    //     ev.preventDefault();
-
-    //     this.props.stripe.confirmCardPayment('{PAYMENT_INTENT_CLIENT_SECRET}', {
-    //         payment_method: {
-    //             card: this.props.elements.getElement('card'),
-    //             billing_details: {
-    //                 name: 'Jenny Rosen',
-    //             },
-    //         }
-    //     })
-    // }
-
-    // render() {
-    //     return (
-    //         <form onSubmit={this.handleSubmit}>
-    //             <CardSection />
-    //             <button>Confirm order</button>
-    //         </form>
-    //     )
-    // }
-
-
+    //cannot figure out how to process customer information into stripe call in backend but all information is correctly being passed to backend******************
     //from newest article
     state ={
-        complete: false
+        complete: false,
+        name: '',
+        email: '',
+        phone: ''
+    }
+
+    handleChange = (event) => {
+        // console.log(event.target.id)
+        this.setState({
+            [event.target.id]: event.target.value
+        })
     }
 
     submit = async () => {
     
         console.log(this.props.orderId)
+        console.log(this.state.name)
         let {token} = await this.props.stripe.createToken({name: 'Name'});
         console.log(token)
         const tokenId = localStorage.token;
@@ -84,7 +39,10 @@ class PaymentForm extends React.Component {
             body: JSON.stringify({
                 token: token.id,
                 orderId: this.props.orderId.data.id,
-                amount: this.props.orderId.data.attributes.total
+                amount: this.props.orderId.data.attributes.total,
+                name: this.state.name,
+                email: this.state.email,
+                phone: this.state.phone,
             })
         })
         console.log(response)
@@ -100,11 +58,61 @@ class PaymentForm extends React.Component {
 
         
         return (
-            <div>
-                <p>Would you like to complete the purchase?</p>
-                <CardSection />
-                <button onClick={this.submit}>Purchase</button>
+            <center>
+            <div id="checkout">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1514517220017-8ce97a34a7b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80" style={{width: '100%', height: '100%', objectFit: 'cover', }} />
+                </div>
+                <div>
+                    <br></br><br></br>
+                    <h2 style={{color: '#6A6C6E'}}>Checkout</h2>
+                    <br></br>
+                    <label id="checkout-label">
+                        <h3>Customer Information</h3>
+                    </label>
+                    
+                    <form id="customer">
+                    <TextField type="text" id="name" label="Full Name" 
+                    style={{width: '50%', color: '#32325D'}} 
+                    value={this.state.name}
+                    onChange={this.handleChange} ></TextField>
+                    
+                    <br></br>
+                    <TextField type="text" id="email" label="Email" 
+                    value={this.state.email} 
+                    onChange={this.handleChange}
+                    style={{width: '50%', color: '#D3D3D3'}} ></TextField>
+                    <br></br>
+                    <TextField type="text" id="phone" label="Phone" 
+                    value={this.state.phone} 
+                    onChange={this.handleChange}
+                    style={{width: '50%', color: '#D3D3D3'}} ></TextField>
+
+                    <br></br><br></br><br></br><br></br>
+                    <CardSection />
+                    <br></br><br></br><br></br>
+                    <div id="payment">
+                        <div>
+                            {(this.props.orderId).length !== 0 ? 
+                            <h3>Total: ${this.props.orderId.data.attributes.total}</h3>
+                            : console.log('not cool')}
+                        </div>
+                        <div>
+                            <Button onClick={this.submit} 
+                            style={{ radius: '3',
+                            border: '0.6px solid #D3D3D3',
+                            backgroundColor: '#dcead9',
+                            color: '#3e4e60',
+                            fontWeight: 'bold',
+                            padding: '10px 25px',
+                            fontFamily: 'Montserrat',}}
+                            >Confirm Purchase</Button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
             </div>
+            </center>
         )
         
     }
